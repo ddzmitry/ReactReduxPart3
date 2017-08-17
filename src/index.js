9dd9723117a4a3a38ca0074ef6624b78f5739ae8
Dzmitry Dubarau <ddzmitry@yahoo.com>
2017-08-17 09:50:34 -0400

    React Router

--- src/index.js
+++ src/index.js
@@ -2,14 +2,39 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
 import { createStore, applyMiddleware } from 'redux';
-
 import App from './components/app';
 import reducers from './reducers';
+// BR I want React look entire URL to tell what to show ! 
+// Route React component that could be rendered inside of ANY components.
 
+import { BrowserRouter , Route } from 'react-router-dom'
 const createStoreWithMiddleware = applyMiddleware()(createStore);
+class Hello extends React.Component{
 
+  render(){
+    return(<div> Hello! World</div>)
+  }
+}
+
+class Bye extends React.Component{
+  
+    render(){
+      return(<div> Good Bye!  </div>)
+    }
+  }
 ReactDOM.render(
+
+
   <Provider store={createStoreWithMiddleware(reducers)}>
-    <App />
+
+      <BrowserRouter>
+        <div>
+        <h1>HEADER</h1>
+      <Route path={'/hello'} component = {Hello}/>
+      <Route path={'/exit'} component = {Bye}/>
+      </div>
+      </BrowserRouter>
   </Provider>
+
+
   , document.querySelector('.container'));
